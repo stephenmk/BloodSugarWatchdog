0baf7b83cfbe072d2af016b32a90ef7fc0556e63
stephenmk <stephenmk@users.noreply.github.com>
2026-06-17 11:30:15 -0500

    Add nav property

--- BloodSugarWatchdog.Data/Entities/TreatmentDevice.cs
+++ BloodSugarWatchdog.Data/Entities/TreatmentDevice.cs
@@ -4,4 +4,6 @@ public sealed class TreatmentDevice
 {
     public required int Id { get; init; }
     public required string Name { get; init; }
+
+    public List<Treatment> Treatments { get; init; } = [];
 }
