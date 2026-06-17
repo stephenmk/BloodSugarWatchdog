d0ff60047b7db5abea05ce8f80dfc44fe384563b
stephenmk <stephenmk@users.noreply.github.com>
2026-06-17 15:20:32 -0500

    Formatting

--- BloodSugarWatchdog.Data/Context.cs
+++ BloodSugarWatchdog.Data/Context.cs
@@ -11,13 +11,16 @@ public sealed class Context : DbContext
     public Context(string username)
     {
         var dirPath = Path.Join(EnvironmentPaths.LocalDataPath, "BloodSugarWatchdog", username);
+
         if (!Directory.Exists(dirPath))
             Directory.CreateDirectory(dirPath);
+
         var builder = new SqliteConnectionStringBuilder
         {
             DataSource = Path.Join(dirPath, "data.db"),
             Pooling = true,
         };
+
         _dbPath = builder.ToString();
     }
 
