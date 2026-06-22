13a928ff2ffe8e174121263bac7157c741196c45
stephenmk <stephenmk@users.noreply.github.com>
2026-06-22 12:37:15 -0500

    Add exception handling for http errors

--- BloodSugarWatchdog.Nightscout/NightscoutService.cs
+++ BloodSugarWatchdog.Nightscout/NightscoutService.cs
@@ -1,6 +1,7 @@
 // Copyright (c) 2026 Stephen Kraus
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+using System.Text.Json.Nodes;
 using BloodSugarWatchdog.Import.Importers;
 using Microsoft.Extensions.Logging;
 
@@ -19,18 +20,34 @@ internal sealed partial class NightscoutService
         while (true)
         {
             var delayTask = Task.Delay(millisecondsDelay, ct);
+            var data = await GetDataAsync(ct);
 
-            var entries = await client.GetEntriesAsync(ct) ?? [];
-            var treatments = await client.GetTreatmentsAsync(ct) ?? [];
-
-            var bglCount = bglImporter.Import(entries);
+            var bglCount = bglImporter.Import(data.Entries);
             LogNewEntries(bglCount);
 
-            var treatmentCount = treatmentImporter.Import(treatments);
+            var treatmentCount = treatmentImporter.Import(data.Treatments);
             LogNewTreatments(treatmentCount);
 
             await delayTask;
         }
+    }
+
+    private sealed record Data(JsonArray Entries, JsonArray Treatments);
+
+    private async Task<Data> GetDataAsync(CancellationToken ct)
+    {
+        JsonArray entries = [];
+        JsonArray treatments = [];
+        try
+        {
+            entries = await client.GetEntriesAsync(ct) ?? [];
+            treatments = await client.GetTreatmentsAsync(ct) ?? [];
+        }
+        catch (HttpRequestException ex)
+        {
+            LogHttpRequestException(ex.Message);
+        }
+        return new(entries, treatments);
     }
 
     [LoggerMessage(LogLevel.Information, "Downloaded {Count:N0} new BGL entries.")]
@@ -38,4 +55,7 @@ internal sealed partial class NightscoutService
 
     [LoggerMessage(LogLevel.Information, "Downloaded {Count:N0} new treatment records.")]
     partial void LogNewTreatments(int count);
+
+    [LoggerMessage(LogLevel.Warning, "HttpRequestException: {Message}")]
+    partial void LogHttpRequestException(string message);
 }
