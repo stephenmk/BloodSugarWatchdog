6c053c62a5a8551b0c01adbec4ca6383dadce2de
stephenmk <stephenmk@users.noreply.github.com>
2026-09-05 14:01:24 -0500

    Formatting

--- Source/BloodSugarWatchdog.DailyReport/Program.cs
+++ Source/BloodSugarWatchdog.DailyReport/Program.cs
@@ -8,24 +8,29 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Logging.AddSystemdConsole();
-builder.Services.AddBloodSugarContext();
-builder.Services.AddPlottingServices();
-builder.Services.AddNotifyService();
+var builder = Host.CreateApplicationBuilder();
 
-using var host = builder.Build();
-var plotter = host.Services.GetRequiredService<IDayPlotter>();
-var notifier = host.Services.GetRequiredService<INotifyService>();
+builder.Logging
+    .AddSystemdConsole();
 
-var path = Path.GetTempFileName();
-plotter.RenderToPath(path);
-var success = await notifier.PostImageAsync(path, caption: args[0]);
+builder.Services
+    .AddBloodSugarContext()
+    .AddPlottingServices()
+    .AddNotifyService();
 
-if (File.Exists(path))
-    File.Delete(path);
+bool success;
 
-if (success)
-    return 0;
-else
-    return 1;
+using (var host = builder.Build())
+{
+    var plotter = host.Services.GetRequiredService<IDayPlotter>();
+    var notify = host.Services.GetRequiredService<INotifyService>();
+
+    var path = Path.GetTempFileName();
+    plotter.RenderToPath(path);
+    success = await notify.PostImageAsync(path, caption: args[0]);
+
+    if (File.Exists(path))
+        File.Delete(path);
+}
+
+return success ? 0 : 1;
